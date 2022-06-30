fc378ca54f0a13259035dc93d618592a61cca211
prtailor11 <prtailor11@gmail.com>
2022-06-30 16:54:23 +0530

    First Commit

---
+++ DemoGit/src/TestGit/AddClass.java
@@ -0,0 +1,11 @@
+package TestGit;
+
+public class AddClass
+{
+	public int addNo(int a,int b)
+	{
+		int c;
+		c = a+b;
+		return c;
+	}
+}
---
+++ DemoGit/src/TestGit/Addition.java
@@ -0,0 +1,13 @@
+package TestGit;
+
+public class Addition {
+
+	public static void main(String[] args) 
+	{
+		AddClass a=new AddClass();
+		System.out.println("Addition is: "+a.addNo(10, 20));
+		
+
+	}
+
+}
